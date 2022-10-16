83c44bece0ad28e7b27accad7e7e81c84e5a00b4
Maria Chrysochoou <t8210168@aueb.gr>
2022-10-16 19:18:34 +0300

    Created new Example.java
    Output.
    
    If you want to add something

---
+++ Example.java
@@ -0,0 +1,7 @@
+public class Example {
+    public void main (String [] args) {
+        System.out.println("Guys, if you want add something here!");
+        System.out.println("Don't forget to update the changes you've made");
+    }
+
+}

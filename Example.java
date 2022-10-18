6d892ac5d6e84e34f5d235eef11baaac705c98e0
Maria Chrysochoou <t8210168@aueb.gr>
2022-10-18 19:26:11 +0300

    Added static

--- Example.java
+++ Example.java
@@ -1,5 +1,5 @@
 public class Example {
-    public void main (String [] args) {
+    public static void main (String [] args) {
         System.out.println("Guys, if you want add something here!");
         System.out.println("Don't forget to update the changes you've made");
     }

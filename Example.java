a005edb5a13333ea26d20e2fb1f0ec6a00b2ad83
ornela13 <ornelakalg@gmail.com>
2022-10-22 16:02:36 +0300

    change

--- Example.java
+++ Example.java
@@ -2,6 +2,8 @@ public class Example {
     public static void main (String [] args) {
         System.out.println("Guys, if you want add something here!");
         System.out.println("Don't forget to update the changes you've made");
+	System.out.println("Hello guys!);
+
     }
 
 }

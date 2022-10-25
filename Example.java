2a4626f9ec1b7c471ed1d98524f6fbb042e4f4c7
Ellie Papastergiou <t8210116@aueb.gr>
2022-10-25 20:01:18 +0300

    changed-file

--- Example.java
+++ Example.java
@@ -2,6 +2,7 @@ public class Example {
     public static void main (String [] args) {
         System.out.println("Guys, if you want add something here!");
         System.out.println("Don't forget to update the changes you've made");
+	System.out.println("Hello!! I'm so happy to be on your team!");
     }
 
 }

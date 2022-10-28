7442d7df887901d193b0bb3c1232c1b0b12f38eb
dimitra03 <t8210147@aueb.gr>
2022-10-28 18:26:09 +0300

    =new line added

--- Example.java
+++ Example.java
@@ -4,6 +4,6 @@ public class Example {
         System.out.println("Don't forget to update the changes you've made");
 	System.out.println("Hello!! I'm so happy to be on your team!");
 	System.out.println("My birthday is on the 29th of April");
+        System.out.println("Hello world!");
     }
-
 }

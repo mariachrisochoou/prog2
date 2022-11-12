03abc9719c46464806a42041ec818a2eefd1461b
Maria Chrysochoou <t8210168@aueb.gr>
2022-11-12 11:11:16 +0200

    Deleted Example.java

--- Example.java
+++
@@ -1,9 +0,0 @@
-public class Example {
-    public static void main (String [] args) {
-        System.out.println("Guys, if you want add something here!");
-        System.out.println("Don't forget to update the changes you've made");
-	System.out.println("Hello!! I'm so happy to be on your team!");
-	System.out.println("My birthday is on the 29th of April");
-        System.out.println("Hello world!");
-    }
-}

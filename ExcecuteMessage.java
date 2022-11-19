d00ce43474300253ce9c5e54be9de2f2a0f222f0
Maria Chrysochoou <t8210168@aueb.gr>
2022-11-19 20:14:09 +0200

    Added comments for better understanding

--- ExcecuteMessage.java
+++ ExcecuteMessage.java
@@ -1,10 +1,15 @@
 public class ExcecuteMessage {
 	
 	public static void main(String [] args) {
+
+		//create a new Message object
 		Message mes = new Message();
 		mes.getMessageDetails();
 		
+		// works in try, catch so there is no kind of exception
 		try {
+
+			// uses the method that requires from the user to give out message details.
 			mes.insertMessageDetails();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());

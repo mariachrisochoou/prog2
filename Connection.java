ed6f79cc55ecaa0856e80bf0001ac862be354475
ornela13 <115373430+ornela13@users.noreply.github.com>
2022-11-22 02:32:06 +0200

    Update Connection.java

--- Connection.java
+++ Connection.java
@@ -1,2 +1,13 @@
 public class Connection {
+  
+    public void executeMessage() {
+        Message mes = new Message();
+        mes.getMessageDetails();
+    
+      try {
+          mes.insertMessageDetails();
+      } catch (Exception e) {
+          System.out.println(e.getMessage());
+      }  
+    }
 }

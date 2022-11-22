e3ef12ab3b7cf45c5f5a7e1e89d45f2f04ff5436
ornela13 <115373430+ornela13@users.noreply.github.com>
2022-11-22 02:26:59 +0200

    Update Messages.java

--- Messages.java
+++ Messages.java
@@ -1,34 +1,4 @@
-import  java.util.Scanner;
+
 public class Messages {
 
-    Scanner input = new Scanner(System.in);
-    public void sendMessage(String receiver) {
-        boolean foundthem = false;
-        for (int i=0; i < users.length; i++) {
-            if users[i]= receiver {
-                 foundthem = true;
-            
-            }
-
-    
-
-        }
-        if (foundthem == true) {
-                String message = input.nextLine();
-                showMessage(String receiver,String message);
-        
-
-
-           
-
-            
-        }
-
-
-    }
-
-    public String showMessage(String receiver,String newmessage){
-        System.out.println("You've got a new message from" + )
-
-    }
 }

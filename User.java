c5744ad83e9cad4012e0323da64e37e20fa72017
Maria Chrysochoou <t8210168@aueb.gr>
2022-11-23 22:59:49 +0200

    Changes
    added getUserDetails(), added getMessageDetails() into Connection.
    Deleted the comments because they made our code very unreadible.

--- User.java
+++ User.java
@@ -13,70 +13,29 @@ import java.util.ArrayList;
 	    private static ArrayList<String> users = new ArrayList<String> ();
 	    
 	    public User(String name, String pw, Calendar dof, String mail) {
-	    	
-	    	//method which checks whether the name exists
 	    	username = name;
-	    	
-	    	//method which checks whether the password has >= 8 numbers
 	    	password = pw;
-	    	
-	    	//If we want we can check whether a user is older than a certain age
 	    	dateOfBirth = dof;
-	    	
-	    	//If we want we can check whether there are same emails
 	    	email = mail;
-	    	
-	    	//If the above requirements are met, then we add the user to our list
-	    	
 	    }
 	    
-	    
-	    //method that checks whether a username is verified
-	    public boolean checkUsername (String name) {
-	    	return true;
-	    }
-	    
-	    
-	    //method that checks whether a pw is longer than 8 numbers
-	    public boolean checkPassword (int pw) {
-	    	return true;
-	    }
-	    
-	    //method that finds if there is another user with the same username
-	    public boolean findUser (String name) {
-	    	return true ;
-	    }
-		
-	    public void getUserDetails() {
-		Scanner input = new Scanner(System.in); //create odject input to get user details
-		System.out.println("Enter your username");
-		username = input.nextLine(); //takes input from the user
-		System.out.println("Enter your email");
-		email = input.nextLine(); //takes input from the user
-		System.out.println("Enter your date of birth");
-		//dateOfBirth = input.nextWHAT(); 
-		System.out.println("Enter your password");
-		password = input.nextLine(); //takes input from the user
-		input.close(); //close input using Scanner's method close() to reclaim memory
-	    }
 		
 	    public void insertUserData() throws Exception {
-		//we open the database connection
-		DbConnection dbconnection = new DbConnection("jdbc:mysql://127.0.0.1:3306/db","test","testforjava"); /* we create dbconnection object,
-		we give as arguments to DbConnection's constructor the url of my local database named db,username = test, password = testforjava 
-		(url,username,password may be different depending if you use your own database in mysql) to successfully connect our program with db*/
-		Connection con = dbconnection.getConnection(); //starts the connection
-		String sql = "INSERT INTO users VALUES(?,?,?,?)";  /*we store the necessary sql statement in a variable sql of type String 
-        	and with the ? are the parameters that will receive a value in lines 31,32,33,34 */ 
-		PreparedStatement stmt = con.prepareStatement(sql); /*we create an object stmt and as argument the sql statement 
-		which gives the ability to call the method setString in order to give to each parameter a value */
-		stmt.setString(1,username); //by calling method setString on object stmt we give the 1st parameter the value of variable username
-		stmt.setString(2,password); //by calling method setString on object stmt we give the 2nd parameter the value of variable password 
-		stmt.setString(3,email); //by calling method setString on object stmt we give the 3rd parameter the value of variable email
-		//stmt.setCalendar(4,dateOfBirth); 
-		stmt.executeUpdate(); //executes the sql statement INSERT INTO..VALUES()
-		System.out.println("Records inserted successfully");
-		DbConnection.closeConnection(con, stmt); //closes connection with the database
+
+			DbConnection dbconnection = new DbConnection("jdbc:mysql://127.0.0.1:3306/db","test","testforjava"); 
+			Connection con = dbconnection.getConnection(); 
+
+			String sql = "INSERT INTO users VALUES(?,?,?,?)"; 
+
+			PreparedStatement stmt = con.prepareStatement(sql); 
+			stmt.setString(1,username); 
+			stmt.setString(2,password); 
+			stmt.setString(3,email);
+			//stmt.setCalendar(4,dateOfBirth); 
+
+			stmt.executeUpdate(); 
+			System.out.println("You have registered successfully!");
+			DbConnection.closeConnection(con, stmt); //closes connection with the database
 	}
 	    
 	}

83cb0c4024dd6e01d812ee9bceddfad61c97b490
christinasiakavara <92878436+christinasiakavara@users.noreply.github.com>
2022-12-13 21:07:38 +0200

    Update logIn.java

--- logIn.java
+++ logIn.java
@@ -1,22 +1,46 @@
-public boolean logIn(String username,String password) {
-    DbConnection dbconnection = new DbConnection("jdbc:mysql://127.0.0.1:3306/db","test","testforjava"); 
-			Connection con = dbconnection.getConnection(); 
-
-     String sql = "SELECT username , password,
+import java.sql.DriverManager;  
+import java.sql.Connection;  
+import java.sql.ResultSet;  
+import java.sql.SQLException;  
+import java.sql.Statement;  
+   
+public class Loguser {  
+	 
+   
+    private Connection connect() {  
+        // SQLite connection string  
+       DbConnection dbconnection = new DbConnection("jdbc:mysql://127.0.0.1:3306/db","test","testforjava"); 
+			Connection con = null;
+        try {  
+            conn = dbconnection.getConnection();  
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        }  
+        return conn;  
+    }  
+   
+  
+    public void selectData(String username,String password)){  
+        String sql = "SELECT username , password,
                    FROM user,
                    WHERE USERNAME = uname";
                    boolean flag;
 
-				   		try {
-				   			PreparedStatement ps = conn.prepareStatement(sql);
-
-				   			ps.setString(1, username);
-				   			ps.setString(2, password);
-				   			flag = true;
-				   		} catch (SQLException e) {
-				   			flag = false;
-				   		} catch (Exception e) {
-				   			flag = false;
-		}
-		return flag;
-	}
+          
+        try {  
+            Connection conn = this.connect();  
+            Statement stmt  = conn.createStatement(sql);  
+            ResultSet ps    = stmt.executeQuery(sql);  
+              
+            // loop through the result set  
+            while (ps.next()) {  
+                System.out.println(rs.getString("username") +  "\t" +   
+                                   rs.getString("password") + "\t" );  
+            }  
+		} catch (Exception e) {
+	           flag = false;
+        } catch (SQLException e) {  
+            flag = false; 
+        }  
+    }  
+      

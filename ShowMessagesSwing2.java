d04711f3b3a0f4e5c12224e5641fc796f8a30b2f
dimitra03 <t8210147@aueb.gr>
2022-12-29 17:54:17 +0200

    updated

--- ShowMessagesSwing2.java
+++ ShowMessagesSwing2.java
@@ -12,95 +12,132 @@ import javax.swing.JScrollPane;
 
 public class ShowMessagesSwing2 implements ActionListener {
 
-	private static JPanel p1,p3;
+	private static JPanel p1,p3,p2;
 	private static JFrame frame;
-	private static JLabel label = new JLabel();
+	private static JLabel label,label2;
 	private static JButton[] b;
-	private static JButton jb;
+	private static JButton back1,back2;
 	private static Message m = new Message(Connect.connect());
 	private static ArrayList<String> senders = new ArrayList<String>();
 	private static ShowMessagesSwing2 sms = new ShowMessagesSwing2(); 
-	
+		
 	public static void createFrame() {
 		frame = new JFrame();
 		frame.setSize(350,350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("My messages");
-        
 	}
-	
+		
 	public static void showMess2(String senderr) {
 		p3 = new JPanel();
 		p3.setLayout(null);
-		frame.add(p3); //
+		frame.add(p3);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		
+			
 		ArrayList<String> messagess = new ArrayList<String>();
-        messagess = m.showMessages(senderr);
-        String[] data = new String[messagess.size()];
-        for (int i = 0; i < data.length; i++) {
-        	data[i] = messagess.get(i);
-        }
-        
-        final DefaultListModel model = new DefaultListModel();
-        for (int i = 0, n = data.length; i < n; i++) {
-          model.addElement(data[i]);
-        }
-        
-        JList jlist = new JList(model);
-        JScrollPane scrollPane1 = new JScrollPane(jlist);
-        frame.add(scrollPane1, BorderLayout.CENTER);
-        
-        jb = new JButton("Return");
-        jb.addActionListener(sms);
-      
-        frame.add(jb, BorderLayout.SOUTH);
-		p3.setVisible(true); //
-        frame.setVisible(true);
-    }
-	
-	public static void start() {
-		p1 = new JPanel();
-		p1.setLayout(null);
-		frame.add(p1); 
-		label = new JLabel("View messages from:");
-		label.setBounds(20, 25, 180, 25);
-		p1.add(label);
+	    messagess = m.showMessages(senderr);
+	    String[] data = new String[messagess.size()];
+	    for (int i = 0; i < data.length; i++) {
+	        data[i] = messagess.get(i);
+	    }
+	        
+	    final DefaultListModel model = new DefaultListModel();
+	    for (int i = 0, n = data.length; i < n; i++) {
+	    	model.addElement(data[i]);
+	    }
+	        
+	    JList jlist = new JList (model);
+	    JScrollPane scrollPane1 = new JScrollPane(jlist);
+	    frame.add(scrollPane1, BorderLayout.CENTER);
+	        
+	    back1 = new JButton("Back");
+	    back1.addActionListener(sms);
+	    p3.add(back1);
+	    frame.add(back1, BorderLayout.SOUTH);
+	    
+	    frame.setVisible(true);
+	}
+		
+	public static boolean checkSenders() {
 		try {
-			senders = m.searchSender("eleni");  //as argument will be the username of the connected user
+			senders = m.searchSender(AppIntro.getName());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		b = new JButton[senders.size()];
-		int y = 50;
-		for (int i = 0; i < b.length; i++) {
-			b[i] = new JButton(senders.get(i));
-			b[i].setBounds(20,y, 150, 25); 
-			b[i].addActionListener(sms);
-			p1.add(b[i]);
-			y+=30;
+			
+		if (senders.size() == 0) { //if there are no senders
+			return false;
+		} else {
+			return true;
 		}
+	}
 		
-		frame.setVisible(true);
-    }
-	
+	public static void start() {
+		if (checkSenders() == true) {
+			p1 = new JPanel();
+			p1.setLayout(null);
+			frame.add(p1); 
+			
+			label = new JLabel("View messages from:");
+			label.setBounds(20, 25, 180, 25);
+			p1.add(label);
+			
+			try {
+				senders = m.searchSender(AppIntro.getName());  
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			b = new JButton[senders.size()];
+			int y = 50;
+			for (int i = 0; i < b.length; i++) {
+				b[i] = new JButton(senders.get(i));
+				b[i].setBounds(20,y, 150, 25); 
+				b[i].addActionListener(sms);
+				p1.add(b[i]);
+				y+=30;
+			}
+			
+			frame.setVisible(true);
+		}
+	}
+		
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		for (int a = 0; a < b.length; a++) {
-			if (e.getSource() == b[a]) {
-				p1.setVisible(false);
-				showMess2(senders.get(a));
-				if (e.getSource() == jb) {
-				    p3.setVisible(false);
-				    p1.setVisible(true);
-				    a = 0;
-				} 
+		if (e.getSource() == back2) {
+			p2.setVisible(false);
+			frame.setVisible(false);
+			MessageSWING.messageOptions();
+		}
+			
+		if (checkSenders() == true) {
+			for (int a = 0; a < b.length; a++) {
+				if (e.getSource() == b[a]) {
+					p1.setVisible(false);
+					showMess2(senders.get(a));
+					if (e.getSource() == back1) {
+						p3.setVisible(false);
+						a = 0;
+					} 
+				}
 			}
 		}
 	}
 	
-	public static void main(String [] args) {
-		createFrame();
-		start();
+	public static void noMessages() {
+		p2 = new JPanel();
+		frame.add(p2);
+		p2.setLayout(null);
+		 
+		back2 = new JButton("Back");
+		back2.setBounds(150, 130, 120, 25);
+		back2.addActionListener(sms);
+		p2.add(back2);
+		
+		label2 = new JLabel("You have no messages");
+		label2.setBounds(60, 40, 180, 25);
+		p2.add(label2);
+		frame.setVisible(true);
 	}
-}
+
+}	
